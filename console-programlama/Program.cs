c932e030fb60e3cc63726cb04d9d26859b6744be
Tarık Aydın <81830855+trkaydn@users.noreply.github.com>
2021-06-24 14:41:20 +0300

    console-programlama

---
+++ console-programlama/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+           Console.WriteLine("isminizi girin");
+            string name = Console.ReadLine();
+            Console.WriteLine("soyisminizi girin");
+            string surname = Console.ReadLine();
+            Console.WriteLine("Merhaba" +" "+name + " " + surname);
+        }
+    }
+}
