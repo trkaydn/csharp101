5845ec7707b69467694e42bc0c5dcc8ae9fbd5de
Tarık Aydın <81830855+trkaydn@users.noreply.github.com>
2021-06-27 13:49:27 +0300

    donguler-while-foreach

---
+++ donguler-while-foreach/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace console_programlama
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            //while
+            //1den başlayarak console'a girilen sayıya kadar (sayı dahil) ortalama hesaplayıp console'a yazdıran program.
+            int sayi = int.Parse(Console.ReadLine());
+            int sayac =0;
+            int toplam=0;
+            while (sayac<=sayi)
+            {
+                toplam+=sayac;
+                sayac++;
+            }
+            Console.WriteLine(toplam/sayi);
+
+            //a'dan z'ye kadar tüm harfleri console'a yazdır.
+            char character='a';
+            while (character <='z')
+            {
+                Console.Write(character);
+                character++;
+            }
+
+            Console.WriteLine("*** Foreach ****");
+            string[] arabalar = {"BMW","Ford","Toyota","Opel"};
+            foreach (var araba in arabalar)
+            {
+                Console.WriteLine(araba);
+            }
+
+
+        }
+    }
+}
