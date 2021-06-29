f38fd909e50d6f031f9f75733dbfdc18daac1b29
Tarık Aydın <81830855+trkaydn@users.noreply.github.com>
2021-06-29 18:51:10 +0300

    csharp101

---
+++ struct-kavrami/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace struct_kavrami
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dikdortgen dikdortgen = new Dikdortgen();
+            dikdortgen.KisaKenar=3;
+            dikdortgen.UzunKenar=4;
+            Console.WriteLine("Class Alan Hesabı: {0}",dikdortgen.AlanHesapla());
+
+            Dikdortgen_Struct dikdortgen_struct = new Dikdortgen_Struct(3,4);
+            //dikdortgen_struct.KisaKenar=3;
+            //dikdortgen_struct.UzunKenar=4;
+            Console.WriteLine("Struct Alan Hesabı: {0}",dikdortgen_struct.AlanHesapla());
+
+        }
+    }
+
+    class Dikdortgen
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen()
+        {
+            KisaKenar=3;
+            UzunKenar=4;
+        }
+
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }
+    }
+
+    struct Dikdortgen_Struct
+    {
+        public int KisaKenar;
+        public int UzunKenar;
+
+        public Dikdortgen_Struct(int kisaKenar,int uzunKenar)
+        {
+            KisaKenar=kisaKenar;
+            UzunKenar=uzunKenar;
+        }
+        public long AlanHesapla()
+        {
+            return this.KisaKenar * this.UzunKenar;
+        }   
+    }
+}
